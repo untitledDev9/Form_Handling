0ae8c029f415646d036238cabb4d8ca2fa665282
untitled <ahmedsuleiman940@gmail.com>
2025-07-01 19:57:32 +0100

    did al the buttons tech

--- src/pages/FormPages.jsx
+++ src/pages/FormPages.jsx
@@ -9,7 +9,10 @@ const FormPages = () => {
     setUsers([...users, { name, email, password, confirm, quote }])
 
     if (name.trim().length < 6) setNameError(true)
-    // if (!email.includes('@')) setNameError('error')
+    if (!email.includes('@')) setEmailError(true);
+    if (password.trim().length < 4) setPasswordError(true);
+    if (confirm !== password) setConfirmError(true);
+
 
     setName("")
     setPassword("")
@@ -22,10 +25,16 @@ const FormPages = () => {
 
   const [name, setName] = useState("")
   const [nameError, setNameError] = useState(false)
+
   const [email, setEmail] = useState("")
-  const [emailError, setEmailError] = useState('')
+  const [emailError, setEmailError] = useState(false)
+
   const [password, setPassword] = useState("")
+  const [passwordError, setPasswordError] = useState(false)
+
   const [confirm, setConfirm] = useState("")
+  const [confirmError, setConfirmError] = useState(false)
+
   const [quote, setQuote] = useState("No Quotes, just be good!!")
 
   const [users, setUsers] = useState([])
@@ -41,24 +50,47 @@ const FormPages = () => {
           <label htmlFor="full_name">Full Name</label>
           <input type="text" id='full_name' placeholder='e.g Untitled Dev' className=' border-2' value={name} onChange={(e) => {
             setName(e.target.value)
-            if (nameError) {
-              setNameError('')
-            }
+            if (nameError) setNameError('')
           }} /> {name}
+          {nameError && (<p>name is too short</p>)}
         </div>
-        {nameError && (<p>name is too short</p>)}
+
+
+
+
         <div>
           <label htmlFor="email">Email</label>
-          <input type="text" id='email' placeholder='joe@gmail.com' className=' border-2' value={email} onChange={(e) => setEmail(e.target.value)} /> {email}
+          <input type="text" id='email' placeholder='joe@gmail.com' className=' border-2' value={email} onChange={(e) => {
+            setEmail(e.target.value)
+            if (emailError) setEmailError("")
+          }} /> {email}
+          {emailError && (<p>put a valid email</p>)}
         </div>
+
+
+
         <div>
           <label htmlFor="password">Password</label>
-          <input type="text" id='password' placeholder='abc12!' className=' border-2' value={password} onChange={(e) => setPassword(e.target.value)} /> {password}
+          <input type="text" id='password' placeholder='abc12!' className=' border-2' value={password} onChange={(e) => {
+            setPassword(e.target.value)
+            if (passwordError) setPasswordError("")
+          }} /> {password}
+          {passwordError && (<p>add more srtdvdv</p>)}
         </div>
+
+
+
         <div>
           <label htmlFor="confirm_password">Confirm Password</label>
-          <input type="text" id='confirm_password' placeholder='same as password' className=' border-2' value={confirm} onChange={(e) => setConfirm(e.target.value)} /> {confirm}
+          <input type="text" id='confirm_password' placeholder='same as password' className=' border-2' value={confirm} onChange={(e) => {
+            setConfirm(e.target.value)
+            if (confirm) setConfirmError("")
+            }} /> {confirm}
+          {confirmError && (<p>password doest match </p>)}
         </div>
+
+
+
         <div>
           <label htmlFor="quote">Quote of the Day</label>
           <textarea id="quote" className=' border-2' value={quote} onChange={(e) => setQuote(e.target.value)} /> {quote}
