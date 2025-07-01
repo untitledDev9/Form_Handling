272f6fc898cd7b47ef0d9698a7742b38ed176a72
untitled <ahmedsuleiman940@gmail.com>
2025-07-01 20:47:38 +0100

    working on styling

--- src/component/FormCard.jsx
+++ src/component/FormCard.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const FormCard = () => {
   return (
-    <div>hhhh</div>
+    <div className=' border w-fit px-5 py-3'>
+      <p>Ahmed Suleiman</p>
+      <p>ahmedsuleiman940@gmail.com</p>
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam in vitae sunt perspiciatis amet aliquid quam optio tenetur odit nihil.
+      </div>
+    </div>
   )
 }
 
--- src/pages/FormPages.jsx
+++ src/pages/FormPages.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import FormCard from '../component/FormCard';
+import backgroungImage from "../assets/undraw_business-man_0ifc.svg";
 
 const FormPages = () => {
 
@@ -42,13 +43,17 @@ const FormPages = () => {
 
   // }
   return (
-    <div>
+    <div className=" flex justify-center items-center h-screen pl-10 pr-32"> 
+
+<div className=' w-[50%] flex items-center justify-center'>
+  <img src={backgroungImage} alt=""  className=' w-[70%]'/>
+</div>
 
 
-      <form onSubmit={handleSubmit} className=' border-2 w-[500px] px-10 bg-amber-300 flex flex-col gap-10 py-10'>
-        <div >
-          <label htmlFor="full_name">Full Name</label>
-          <input type="text" id='full_name' placeholder='e.g Untitled Dev' className=' border-2' value={name} onChange={(e) => {
+      <form onSubmit={handleSubmit} className=' w-[50%] px-10 shadow-[0_10px_60px_rgba(23,184,166,0.7)] flex flex-col gap-10 py-10 rounded-2xl'>
+        <div  className='flex flex-col gap-2'>
+          <label htmlFor="full_name">Full Name:</label>
+          <input type="text" id='full_name' placeholder='e.g Untitled Dev' className=' border-b-2 h-10' value={name} onChange={(e) => {
             setName(e.target.value)
             if (nameError) setNameError('')
           }} /> {name}
@@ -58,7 +63,7 @@ const FormPages = () => {
 
 
 
-        <div>
+        <div className='flex flex-col'>
           <label htmlFor="email">Email</label>
           <input type="text" id='email' placeholder='joe@gmail.com' className=' border-2' value={email} onChange={(e) => {
             setEmail(e.target.value)
@@ -69,7 +74,7 @@ const FormPages = () => {
 
 
 
-        <div>
+        <div className='flex flex-col'>
           <label htmlFor="password">Password</label>
           <input type="text" id='password' placeholder='abc12!' className=' border-2' value={password} onChange={(e) => {
             setPassword(e.target.value)
@@ -80,7 +85,7 @@ const FormPages = () => {
 
 
 
-        <div>
+        <div className='flex flex-col'>
           <label htmlFor="confirm_password">Confirm Password</label>
           <input type="text" id='confirm_password' placeholder='same as password' className=' border-2' value={confirm} onChange={(e) => {
             setConfirm(e.target.value)
@@ -91,9 +96,9 @@ const FormPages = () => {
 
 
 
-        <div>
+        <div className='flex flex-col'>
           <label htmlFor="quote">Quote of the Day</label>
-          <textarea id="quote" className=' border-2' value={quote} onChange={(e) => setQuote(e.target.value)} /> {quote}
+          <textarea id="quote" className=' border-2 max-h-[100px]' value={quote} onChange={(e) => setQuote(e.target.value)}/> {quote}
         </div>
         <button className=' border-2' type='submit'>Submit</button>
       </form>
@@ -104,7 +109,7 @@ const FormPages = () => {
           <div key={index}>
           </div>
           ))} */}
-      <FormCard />
+      {/* <FormCard /> */}
     </div>
   )
 }
