e29104590baba25a426399657620f4d7b1e6a591
untitled <ahmedsuleiman940@gmail.com>
2025-07-02 02:47:14 +0100

    done

--- src/component/FormCard.jsx
+++ src/component/FormCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const FormCard = ({name, email, quote}) => {
   return (
-    <div className=' bg-[#17B8A6] text-white w-[270px] h-[274px] flex flex-col py-4 items-center'>
+    <div className=' bg-[#17B8A6] text-white w-[270px] h-[274px] py-4 items-center rounded-[10px]'>
       <h2 className=' font-semibold text-[25px] mb-5'>{name}</h2>
       <h2 className=' text-[10px] text-blue-600 font-bold'>Contact</h2>
       <h3 className=' text-[14px]'>{email}</h3>
--- src/pages/FormPages.jsx
+++ src/pages/FormPages.jsx
@@ -20,155 +20,160 @@ const FormPages = () => {
   const [quote, setQuote] = useState("No Quotes, just be good!!")
 
   const [users, setUsers] = useState([])
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
     let isValid = true;
 
 
-    setNameError (false)
-    setEmailError (false)
-    setPasswordError (false)
-    setConfirmError (false)
+    setNameError(false)
+    setEmailError(false)
+    setPasswordError(false)
+    setConfirmError(false)
 
-    if (name.trim().length <= 6){
+    if (name.trim().length <= 6) {
       setNameError(true);
-    isValid = false
-  };
-  if (!email.includes("@")) {
-    setEmailError(true)
-    isValid = false
+      isValid = false
+    };
+    if (!email.includes("@")) {
+      setEmailError(true)
+      isValid = false
+    }
+    if (password.trim().length < 4) {
+      setPasswordError(true)
+      isValid = false
+    }
+    if (confirm !== password) {
+      setConfirmError(true)
+      isValid = false
+    }
+
+    if (!isValid) return;
+
+    setUsers([...users, { name, email, password, confirm, quote }])
+
+    console.log("Glitch Submitted the Form :", name, email, password, confirm);
+
+    setName("");
+    setEmail("");
+    setPassword("");
+    setConfirm("");
+    setQuote("No Quotes, just be Good")
+
   }
-  if (password.trim().length < 4) {
-    setPasswordError(true)
-    isValid = false
-  }
-  if (confirm !== password) {
-    setConfirmError(true)
-    isValid = false
-  }
-
-  if (!isValid) return;
-
-  setUsers([...users, { name, email, password, confirm, quote }])
-
-  console.log("Glitch Submitted the Form :", name, email, password, confirm);
-
-  setName("");
-  setEmail("");
-  setPassword("");
-  setConfirm("");
-  setQuote("No QUotes, just be Good Blessing")
-
-}
 
   return (
     <>
-    
-
-    <section className=" flex justify-center items-center h-screen pl-10 pr-32 mb-20">
-
-      <main className=' w-[50%] flex items-center justify-center'>
-        <img src={backgroungImage} alt="" className=' w-[70%]' />
-      </main>
 
 
-      <form onSubmit={handleSubmit} className=' w-[50%] px-10 shadow-[0_10px_60px_rgba(23,184,166,0.7)] flex flex-col gap-10 py-10 rounded-2xl'>
-        <div>
+      <section className=" flex justify-center items-center h-screen pl-10 pr-32 mb-20">
+
+        <main className=' w-[50%] flex items-center justify-center'>
+          <img src={backgroungImage} alt="" className=' w-[70%]' />
+        </main>
 
 
-
-          <label htmlFor="full_name">Full Name:</label>
-          <input type="text" id='full_name' placeholder='e.g Untitled Dev' value={name} onChange={(e) => {
-            setName(e.target.value)
-            if (nameError) {
-              setNameError(false)
-    
-            }
-          }}
-            className=' border-2 input focus:outline-none focus:border-0 focus:border-b-2 focus:border-[#17B8A6] placeholder:text-[#6969698b] placeholder:text-[13px] placeholder:font-light transition-all duration-200 ease-in'
-          />
-          {nameError && (<p>Name is too short 🤦‍♂️</p>)}
-        </div>
+        <form onSubmit={handleSubmit} className=' w-[50%] px-10 shadow-[0_10px_60px_rgba(23,184,166,0.7)] flex flex-col gap-10 py-10 rounded-2xl'>
+          <div>
 
 
 
+            <label htmlFor="full_name">Full Name:</label>
+            <input type="text" id='full_name' placeholder='e.g Untitled Dev' value={name} onChange={(e) => {
+              setName(e.target.value)
+              if (nameError) {
+                setNameError(false)
 
-        <div className='flex flex-col'>
-          <label htmlFor="email">Email</label>
-          <input type="text" id='email' placeholder='joe@gmail.com' value={email} onChange={(e) => {
-            setEmail(e.target.value)
-            if (emailError) {
-             
-              setEmailError(false)
-             
-            }
-          }}
-            className=' border-2 input focus:outline-none focus:border-0 focus:border-b-2 focus:border-[#17B8A6] placeholder:text-[#6969698b] placeholder:text-[13px] placeholder:font-light transition-all duration-200 ease-in'
-          />
-          {emailError && (<p>Please enter a valid email</p>)}
-        </div>
+              }
+            }}
+              className=' border-2 input focus:outline-none focus:border-0 focus:border-b-2 focus:border-[#17B8A6] placeholder:text-[#6969698b] placeholder:text-[13px] placeholder:font-light transition-all duration-200 ease-in'
+            />
+            {nameError && (<p>Name is too short 🤦‍♂️</p>)}
+          </div>
 
 
 
-        <div className='flex flex-col'>
-          <label htmlFor="password">Password</label>
-          <input type="text" id='password' placeholder='abc12!' value={password} onChange={(e) => {
-            setPassword(e.target.value)
-            if (passwordError) {
-            
-              setPasswordError(false)
-             
-            }
-          }}
-            className=' border-2 input focus:outline-none focus:border-0 focus:border-b-2 focus:border-[#17B8A6] placeholder:text-[#6969698b] placeholder:text-[13px] placeholder:font-light transition-all duration-200 ease-in'
-          />
-          {passwordError && (<p>Password must be 6 characters long</p>)}
-        </div>
+
+          <div className='flex flex-col'>
+            <label htmlFor="email">Email</label>
+            <input type="text" id='email' placeholder='joe@gmail.com' value={email} onChange={(e) => {
+              setEmail(e.target.value)
+              if (emailError) {
+
+                setEmailError(false)
+
+              }
+            }}
+              className=' border-2 input focus:outline-none focus:border-0 focus:border-b-2 focus:border-[#17B8A6] placeholder:text-[#6969698b] placeholder:text-[13px] placeholder:font-light transition-all duration-200 ease-in'
+            />
+            {emailError && (<p>Please enter a valid email</p>)}
+          </div>
 
 
 
-        <div className='flex flex-col'>
-          <label htmlFor="confirm_password">Confirm Password</label>
-          <input type="text" id='confirm_password' placeholder='same as password' value={confirm} onChange={(e) => {
-            setConfirm(e.target.value)
-            if (confirm) {
-              
-              setConfirmError(false)
-            }
-          }}
-            className=' border-2 input focus:outline-none focus:border-0 focus:border-b-2 focus:border-[#17B8A6] placeholder:text-[#6969698b] placeholder:text-[13px] placeholder:font-light transition-all duration-200 ease-in'
-          />
-          {confirmError && (<p>password does'nt match !! </p>)}
-        </div>
+          <div className='flex flex-col'>
+            <label htmlFor="password">Password</label>
+            <input type="text" id='password' placeholder='abc12!' value={password} onChange={(e) => {
+              setPassword(e.target.value)
+              if (passwordError) {
+
+                setPasswordError(false)
+
+              }
+            }}
+              className=' border-2 input focus:outline-none focus:border-0 focus:border-b-2 focus:border-[#17B8A6] placeholder:text-[#6969698b] placeholder:text-[13px] placeholder:font-light transition-all duration-200 ease-in'
+            />
+            {passwordError && (<p>Password must be 6 characters long</p>)}
+          </div>
 
 
 
-        <div className='flex flex-col'>
-          <label htmlFor="quote">Quote of the Day</label>
-          <textarea id="quote" className='border-2 h-[60px] focus:border-[#17B8A6] max-h-[100px]' value={quote} onChange={(e) => setQuote(e.target.value)} />
-        </div>
-        <button className=' border-2' type='submit'>Submit</button>
-      {/* <Link to='/form-card' >
+          <div className='flex flex-col'>
+            <label htmlFor="confirm_password">Confirm Password</label>
+            <input type="text" id='confirm_password' placeholder='same as password' value={confirm} onChange={(e) => {
+              setConfirm(e.target.value)
+              if (confirm) {
+
+                setConfirmError(false)
+              }
+            }}
+              className=' border-2 input focus:outline-none focus:border-0 focus:border-b-2 focus:border-[#17B8A6] placeholder:text-[#6969698b] placeholder:text-[13px] placeholder:font-light transition-all duration-200 ease-in'
+            />
+            {confirmError && (<p>password does'nt match !! </p>)}
+          </div>
+
+
+
+          <div className='flex flex-col'>
+            <label htmlFor="quote">Quote of the Day</label>
+            <textarea id="quote" className='border-2 h-[60px] focus:border-[#17B8A6] max-h-[100px]' value={quote} onChange={(e) => setQuote(e.target.value)} />
+          </div>
+          <button className=' border-2' type='submit'>Submit</button>
+          {/* <Link to='/form-card' >
             <h2 className=' text-[#fff] font-semibold bg-[#17B8A6] py-2 rounded text-center mx-auto'>View all registerd form</h2>
       </Link> */}
-      </form>
+        </form>
 
 
 
 
-    </section>
-      {users.map((data, index) => (
-          <div key={index} className=' px-18 py-10'>
-            <FormCard 
-            name={data.name}
-            email={data.email}
-            quote={data.quote}
-            />
-          </div>
-          ))}
+      </section>
 
-        </>
+      
+<section className='  flex gap-22 flex-wrap items-center justify-center py-18'>
+    {users.map((data, index) => (
+      <section key={index} className="w-fit">
+        <FormCard
+          name={data.name}
+          email={data.email}
+          quote={data.quote}
+        />
+      </section>
+    ))}
+</section>
+
+
+    </>
   )
 }
 
